1846277f7efc47a8c69d8611cdb63bbdc2db39a8
debindv <debinptpm@gmail.com>
2020-04-24 12:49:47 +0530

    Adjustment

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,6 @@
 const HDwalletProvider = require('@truffle/hdwallet-provider');
 require('dotenv').config();
-var mnemonic = process.env.MNEMONIC;
-var key = process.env.INFURA_API_KEY;
+
 module.exports = {
     // See <http://truffleframework.com/docs/advanced/configuration>
     // for more about customizing your Truffle configuration!
@@ -13,8 +12,7 @@ module.exports = {
       //   gas: 6000000
       // },
       rinkeby: {
-        provider: () => new HDwalletProvider(mnemonic, "https://rinkeby.infura.io/v3/"+key),
-        // provider: () => new HDwalletProvider(process.env.MNEMONIC, "https://rinkeby.infura.io/v3/24b49cc800a04404ae669233b6931097"),
+        provider: () => new HDwalletProvider("stand toss wide over huge predict razor prepare suffer sight wise bar", "https://rinkeby.infura.io/v3/24b49cc800a04404ae669233b6931097"),
         network_id: 4
       }
 
