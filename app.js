70e1314cf7fa99de157f028b22282a5f23268928
debindv <debinptpm@gmail.com>
2020-04-24 11:21:00 +0530

    API configuration

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var Web3 = require("web3");
 const flash = require('connect-flash');
 const Email = require('./models/Email');
 var helmet = require('helmet');
-require('dotenv').config();
+//require('dotenv').config();
 
 web3 = new Web3("https://rinkeby.infura.io/v3/"+process.env.INFURA_API_KEY);
 //web3 = new Web3("http://localhost:8545");
--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,5 @@
 const HDwalletProvider = require('@truffle/hdwallet-provider');
-require('dotenv').config();
+//require('dotenv').config();
 module.exports = {
     // See <http://truffleframework.com/docs/advanced/configuration>
     // for more about customizing your Truffle configuration!
