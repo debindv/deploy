f4bc5d2dec67d9907aa06576e8eec7aad51a5c6d
debindv <debinptpm@gmail.com>
2020-04-25 15:47:19 +0530

    Fixed Key issues

---
+++ bs-config.js
@@ -0,0 +1,7 @@
+// module.exports = {
+//     port: process.env.PORT,
+//     files: ["./front-end.{html,css,png}","./routes.{js}","./views.{ejs}"],
+//     server: {
+//         baseDir: ["./","./build/contracts"]
+//     }
+// }
