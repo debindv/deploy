9cf67c7768a1174be0cdbef841c5e60cc3780d61
DEBIN D VARGHESE <debinptpm@gmail.com>
2020-04-26 18:09:12 +0530

    Deleted Bs-config file

--- bs-config.js
+++
@@ -1,7 +0,0 @@
-// module.exports = {
-//     port: process.env.PORT,
-//     files: ["./front-end.{html,css,png}","./routes.{js}","./views.{ejs}"],
-//     server: {
-//         baseDir: ["./","./build/contracts"]
-//     }
-// }
