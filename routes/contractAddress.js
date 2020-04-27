79afeee4b8682cdaadb9e608d2ad00614ea7a62b
debindv <debinptpm@gmail.com>
2020-04-27 20:37:44 +0530

    completed admin functionalities

--- routes/contractAddress.js
+++ routes/contractAddress.js
@@ -5,14 +5,15 @@ const path = require('path');
 
 
 router.post('/', (req,res) => {
-    var caddr = //CONTRACT ADDRESS FROM EJS
-    // contractAddress = caddr;
-    console.log(`COINBASE NEW ADDRESS ${caddr}`);
-    // const contractAbi = require('./contracts/contractAbi');
+    var caddr = req.body.contractAddress;
+    //contractAddress = caddr;
+    // console.log(`COINBASE NEW ADDRESS ${caddr}`);
+    const contractAbi = require('./contracts/contractAbi');
 
     // Election = new web3.eth.Contract(
     //     contractAbi, contractAddress
     // );
+    res.render('admin_dashboard');
 });
 
 
