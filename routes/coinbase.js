ff6dcc83a716baade543d6223c930c0732785dba
debindv <debinptpm@gmail.com>
2020-04-27 21:26:06 +0530

    Added admin login

--- routes/coinbase.js
+++ routes/coinbase.js
@@ -6,7 +6,7 @@ const path = require('path');
 router.post('/' , (req,res) => {
     var address = req.body.coinbase;
     console.log(`COINBASE NEW ADDRESS ${address}`);
-    coinbase = address;
+    //coinbase = address;
     //alert('COINBASE UPDATED');
     res.render('admin_dashboard');
 });
